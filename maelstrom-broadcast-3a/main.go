c43e9a564a20aedce87b351988153c28c0139f89
Luke Nguyen <luke_nguyen@mckinsey.com>
2023-03-15 12:59:41 +0700

    refactor: challenge 3a broadcast single node

--- maelstrom-broadcast-3a/main.go
+++ maelstrom-broadcast-3a/main.go
@@ -3,46 +3,68 @@ package main
 import (
 	"encoding/json"
 	"log"
+
 	"os"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
+type server struct {
+	n      *maelstrom.Node
+	values []float64
+}
+
+func (s *server) HandleRead(msg maelstrom.Message) error {
+	var request map[string]any
+	if err := json.Unmarshal(msg.Body, &request); err != nil {
+		return err
+
+	}
+	var response = map[string]any{
+		"type":     "read_ok",
+		"messages": s.values,
+	}
+	return s.n.Reply(msg, response)
+}
+
+func (s *server) HandleBroadcast(msg maelstrom.Message) error {
+	var request map[string]any
+	if err := json.Unmarshal(msg.Body, &request); err != nil {
+		return err
+	}
+	s.values = append(s.values, request["message"].(float64))
+	var response = map[string]any{
+		"type": "broadcast_ok",
+	}
+	return s.n.Reply(msg, response)
+}
+
+func (s *server) HandleTopology(msg maelstrom.Message) error {
+	var request map[string]any
+	if err := json.Unmarshal(msg.Body, &request); err != nil {
+		return err
+	}
+	var response = map[string]any{
+		"type": "topology_ok",
+	}
+	return s.n.Reply(msg, response)
+}
+
 func main() {
 	n := maelstrom.NewNode()
-	var values = []float64{}
+	s := &server{n: n}
 
-	n.Handle("broadcast", func(msg maelstrom.Message) error {
-		var body map[string]any
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
-		}
-		value := body["message"].(float64)
-		values = append(values, value)
-		body["type"] = "broadcast_ok"
-		delete(body, "message")
-		return n.Reply(msg, body)
-	})
+	go func() {
+		n.Handle("broadcast", s.HandleBroadcast)
+	}()
 
-	n.Handle("read", func(msg maelstrom.Message) error {
-		var body map[string]any
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
-		}
-		body["type"] = "read_ok"
-		body["messages"] = values
-		return n.Reply(msg, body)
-	})
+	go func() {
+		n.Handle("read", s.HandleRead)
+	}()
 
-	n.Handle("topology", func(msg maelstrom.Message) error {
-		var body map[string]any
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
-		}
-		body["type"] = "topology_ok"
-		delete(body, "topology")
-		return n.Reply(msg, body)
-	})
+	go func() {
+		n.Handle("topology", s.HandleTopology)
+	}()
 
 	// Execute the node's message loop. This will run until STDIN is closed.
 	if err := n.Run(); err != nil {
